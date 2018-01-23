5f11f4bdc4eb7be9fb6c84cb808339955824ef35
Daniel Zhu <danielzhu777@gmail.com>
2018-01-23 16:19:17 -0600

    Added Album Art and Buy Button

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -2,20 +2,29 @@ import React from 'react';
 import {Text, View, Image} from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
 const AlbumDetail = (props) => {
   return (
       <Card>
         <CardSection>
-          <View>
+          <View style = {styles.thumbnailContainerStyle}>
             <Image
               style = {styles.thumbnailStyle}
               source = {{uri: props.data.thumbnail_image }} />
           </View>
           <View style = {styles.headerContentStyle}>
-            <Text>{props.data.title}</Text>
+            <Text style = {styles.headerTextStyle}>{props.data.title}</Text>
             <Text>{props.data.artist}</Text>
           </View>
+        </CardSection>
+        <CardSection>
+          <Image
+            style = {styles.imageStyle}
+            source = {{uri: props.data.image}}/>
+        </CardSection>
+        <CardSection>
+          <Button onPress = { }/>
         </CardSection>
       </Card>
   );
@@ -26,9 +35,23 @@ const styles = {
     flexDirection: 'column',
     justifyContent: 'space-around'
   },
+  headerTextStyle: {
+    fontSize: 18
+  },
   thumbnailStyle: {
     height: 50,
     width: 50
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
   }
 };
 
