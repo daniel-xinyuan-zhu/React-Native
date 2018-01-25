7cc9b5044373577d00ec70189db79f001a48e369
Daniel Zhu <danielzhu777@gmail.com>
2018-01-25 16:20:05 -0600

    Implemented logout functionality

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,12 +1,12 @@
 import React, {Component } from 'react';
 import {View, Text} from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, Spinner} from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
     state = {
-      loggedIn: false
+      loggedIn: null
     };
   componentWillMount() {
     firebase.initializeApp({
@@ -25,11 +25,27 @@ class App extends Component {
       }
     });
   }
+
+  renderContent() {
+    switch (this.state.loggedIn)
+    {
+      case true:
+          return (
+            <Button onPress = { () => firebase.auth().signOut()} >
+              Log Out
+            </Button>);
+      case false:
+          return <LoginForm />;
+      default:
+          return <Spinner size = "large" />;
+    }
+  }
+
   render() {
     return(
       <View>
         <Header headerText = "Authentication" />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
